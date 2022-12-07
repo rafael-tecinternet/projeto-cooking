4e684b5f09c1e5b64e936945dc9a6af963da8a7d
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2022-12-07 11:10:28 -0300

    Instalação de extenções

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Home from "./src/screen/Home";
 
-export default function App() {
+const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      {/* O NavigationContainer deve envolver todas as telas navegáveis do nosso APP. */}
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#5451a6",
+            },
+            headerTintColor: "white",
+          }}
+        >
+          <Stack.Screen
+            component={Home}
+            name="Home"
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
-}
+};
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const estilos = StyleSheet.create({});
