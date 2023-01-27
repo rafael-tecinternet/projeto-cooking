0d05a4a0c2768d342b51021e4701de7f6d815d87
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2023-01-27 11:53:41 -0300

    commit

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,10 +1,9 @@
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import serverApi from '../services/api';
-import { useParams } from 'react-router-dom';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import serverApi from "../services/api";
+import { useParams } from "react-router-dom";
 
 const Home = () => {
-
   const [receitas, setReceitas] = useState([]);
 
   useEffect(() => {
@@ -12,39 +11,34 @@ const Home = () => {
       try {
         const resposta = await fetch(`${serverApi}/receitas.json`);
         const dados = await resposta.json();
+        console.log(receitas);
         setReceitas(dados);
       } catch (error) {
         console.log("Deu ruim! " + error.message);
       }
     }
     getReceitas();
-    
   }, []);
 
-
-  console.log(receitas);
   return (
-    
     <SafeAreaView style={estilos.container}>
       <View>
         <View style={estilos.titulo}>{receitas.id}</View>
         <Text> </Text>
       </View>
     </SafeAreaView>
-    
-  )
-}
+  );
+};
 
 export default Home;
 
 const estilos = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   titulo: {
     color: "black",
     fontWeight: "bold",
     fontSize: 16,
   },
-  
-})
+});
