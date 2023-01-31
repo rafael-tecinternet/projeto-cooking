c57a25df4c71d4d9d1e3c4c61d47c3c2f04f2912
Matheus Anthony <thehellc4t@gmail.com>
2023-01-31 11:58:55 -0300

    adicionados icones e anotações

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -21,19 +21,17 @@ const Home = () => {
   return (
     <SafeAreaView style={estilos.container}>
       <View style={estilos.viewForm}>
-      
+      {/* faz com que o icone de hamburguer (reorder) fique dentro do formulario (não funcionando ainda) */}
+      <Pressable>
+        <Ionicons name="reorder-three" size={30} color="black" />
+        </Pressable>
         <TextInput 
         style={estilos.barraDePesquisa}
         placeholder="Pesquisar ingredientes"/>
         
-        <Pressable>
-        <Ionicons name="reorder-three" size={30} color="black" />
-        </Pressable>
-
-
         <View>
         
-        {/* adicionar onpress no pressable e a programação */}
+        {/* a fazer: adicionar onpress no pressable e a programação para ir para os detalhes da receita */}
         <Pressable>
         <ImageBackground style={estilos.BlocosDeConteudo}
         source={ImagemAlternativa}>
@@ -68,19 +66,17 @@ const estilos = StyleSheet.create({
     marginBottom: 40,
     paddingHorizontal: 65, // extende a barra de pesquisa para os lados (horizontal)
     paddingVertical: 5,
-    
   }, BlocosDeConteudo: {
     // Aqui ele irá afetar os blocos de conteudo
     marginVertical: 15,
     backgroundColor: "gray",
-    fontSize: 15,
     borderRadius: 5, // arredonda a borda
-    width: "100%",
+    width: "100%", // extende em 100% o bloco de conteudo
     height: 150,
     alignItems: "center",
     justifyContent: "center",
     paddingTop: 15, // move o texto um pouco mais pra baixo
-    paddingBottom: 80, // 
+    paddingBottom: 80, 
     borderWidth: 3, // aumenta a grossura da borda
     overflow: "hidden", // faz com que a imagem não passe da borda
   }, texto: {
