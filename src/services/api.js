717f1d90dff31fcf8ebca3d39d670f305338ec6d
Matheus Anthony <thehellc4t@gmail.com>
2023-02-06 08:28:05 -0300

    atualizado codigo do serverApi

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,3 @@
-import axios from "axios";
-const api = axios.create({
-  baseURL: "https://mobile-975e2-default-rtdb.firebaseio.com/",
-});
 
-export default api;
+const serverApi = `https://mobile-975e2-default-rtdb.firebaseio.com/`;
+export default serverApi;
