9f0cbff7e0aa082626493bf8c13b182f004d7838
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2023-02-07 09:48:05 -0300

    tela detalhe

---
+++ src/screens/Detalhes.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Detalhes = () => {
+  return (
+    <View>
+      <Text>Detalhes</Text>
+    </View>
+  );
+};
+
+export default Detalhes;
+
+const styles = StyleSheet.create({});
